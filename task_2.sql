26a4916022d67f3f42bbceae7100bf33d6a76510
Wagnew Moges <149929587+wagnewAI@users.noreply.github.com>
2025-08-15 12:07:28 +0300

    Update task_2.sql

--- task_2.sql
+++ task_2.sql
@@ -1,1 +1,45 @@
+-- Use the database
+USE alx_book_store;
+
+-- Create AUTHORS table
+CREATE TABLE IF NOT EXISTS Authors (
+    author_id INT PRIMARY KEY AUTO_INCREMENT,
+    author_name VARCHAR(215) NOT NULL
+);
+
+-- Create BOOKS table
+CREATE TABLE IF NOT EXISTS Books (
+    book_id INT PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(130) NOT NULL,
+    author_id INT NOT NULL,
+    price DOUBLE NOT NULL,
+    publication_date DATE,
+    FOREIGN KEY (author_id) REFERENCES Authors(author_id)
+);
+
+-- Create CUSTOMERS table
+CREATE TABLE IF NOT EXISTS Customers (
+    customer_id INT PRIMARY KEY AUTO_INCREMENT,
+    customer_name VARCHAR(215) NOT NULL,
+    email VARCHAR(215) UNIQUE NOT NULL,
+    address TEXT
+);
+
+-- Create ORDERS table
+CREATE TABLE IF NOT EXISTS Orders (
+    order_id INT PRIMARY KEY AUTO_INCREMENT,
+    customer_id INT NOT NULL,
+    order_date DATE NOT NULL,
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
+);
+
+-- Create ORDER_DETAILS table
+CREATE TABLE IF NOT EXISTS Order_Details (
+    orderdetailid INT PRIMARY KEY AUTO_INCREMENT,
+    order_id INT NOT NULL,
+    book_id INT NOT NULL,
+    quantity DOUBLE NOT NULL,
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
+);
 
